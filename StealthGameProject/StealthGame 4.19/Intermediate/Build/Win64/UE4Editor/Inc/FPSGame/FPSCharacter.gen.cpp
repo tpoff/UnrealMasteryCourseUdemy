c2422551850abeb19dd527cdef724f1408255be0
Tyler McGraw Poff <tyler.poff@outlook.com>
2018-10-01 07:03:48 -0500

    Added stealth game base.
    added the base project files provided by the course.

---
+++ StealthGameProject/StealthGame 4.19/Intermediate/Build/Win64/UE4Editor/Inc/FPSGame/FPSCharacter.gen.cpp
@@ -0,0 +1,133 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "Public/FPSCharacter.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeFPSCharacter() {}
+// Cross Module References
+	FPSGAME_API UClass* Z_Construct_UClass_AFPSCharacter_NoRegister();
+	FPSGAME_API UClass* Z_Construct_UClass_AFPSCharacter();
+	ENGINE_API UClass* Z_Construct_UClass_ACharacter();
+	UPackage* Z_Construct_UPackage__Script_FPSGame();
+	ENGINE_API UClass* Z_Construct_UClass_UAnimSequence_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_USoundBase_NoRegister();
+	COREUOBJECT_API UClass* Z_Construct_UClass_UClass();
+	FPSGAME_API UClass* Z_Construct_UClass_AFPSProjectile_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_USkeletalMeshComponent_NoRegister();
+// End Cross Module References
+	void AFPSCharacter::StaticRegisterNativesAFPSCharacter()
+	{
+	}
+	UClass* Z_Construct_UClass_AFPSCharacter_NoRegister()
+	{
+		return AFPSCharacter::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AFPSCharacter()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_ACharacter,
+				(UObject* (*)())Z_Construct_UPackage__Script_FPSGame,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "HideCategories", "Navigation" },
+				{ "IncludePath", "FPSCharacter.h" },
+				{ "ModuleRelativePath", "Public/FPSCharacter.h" },
+			};
+#endif
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_FireAnimation_MetaData[] = {
+				{ "Category", "Gameplay" },
+				{ "ModuleRelativePath", "Public/FPSCharacter.h" },
+				{ "ToolTip", "AnimMontage to play each time we fire" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_FireAnimation = { UE4CodeGen_Private::EPropertyClass::Object, "FireAnimation", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000010001, 1, nullptr, STRUCT_OFFSET(AFPSCharacter, FireAnimation), Z_Construct_UClass_UAnimSequence_NoRegister, METADATA_PARAMS(NewProp_FireAnimation_MetaData, ARRAY_COUNT(NewProp_FireAnimation_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_FireSound_MetaData[] = {
+				{ "Category", "Gameplay" },
+				{ "ModuleRelativePath", "Public/FPSCharacter.h" },
+				{ "ToolTip", "Sound to play each time we fire" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_FireSound = { UE4CodeGen_Private::EPropertyClass::Object, "FireSound", RF_Public|RF_Transient|RF_MarkAsNative, 0x0010000000010001, 1, nullptr, STRUCT_OFFSET(AFPSCharacter, FireSound), Z_Construct_UClass_USoundBase_NoRegister, METADATA_PARAMS(NewProp_FireSound_MetaData, ARRAY_COUNT(NewProp_FireSound_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_ProjectileClass_MetaData[] = {
+				{ "Category", "Projectile" },
+				{ "ModuleRelativePath", "Public/FPSCharacter.h" },
+				{ "ToolTip", "Projectile class to spawn" },
+			};
+#endif
+			static const UE4CodeGen_Private::FClassPropertyParams NewProp_ProjectileClass = { UE4CodeGen_Private::EPropertyClass::Class, "ProjectileClass", RF_Public|RF_Transient|RF_MarkAsNative, 0x0014000000010001, 1, nullptr, STRUCT_OFFSET(AFPSCharacter, ProjectileClass), Z_Construct_UClass_AFPSProjectile_NoRegister, Z_Construct_UClass_UClass, METADATA_PARAMS(NewProp_ProjectileClass_MetaData, ARRAY_COUNT(NewProp_ProjectileClass_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_CameraComponent_MetaData[] = {
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "Public/FPSCharacter.h" },
+				{ "ToolTip", "First person camera" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_CameraComponent = { UE4CodeGen_Private::EPropertyClass::Object, "CameraComponent", RF_Public|RF_Transient|RF_MarkAsNative, 0x00200800000a001d, 1, nullptr, STRUCT_OFFSET(AFPSCharacter, CameraComponent), Z_Construct_UClass_UCameraComponent_NoRegister, METADATA_PARAMS(NewProp_CameraComponent_MetaData, ARRAY_COUNT(NewProp_CameraComponent_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_GunMeshComponent_MetaData[] = {
+				{ "Category", "Mesh" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "Public/FPSCharacter.h" },
+				{ "ToolTip", "Gun mesh: 1st person view (seen only by self)" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_GunMeshComponent = { UE4CodeGen_Private::EPropertyClass::Object, "GunMeshComponent", RF_Public|RF_Transient|RF_MarkAsNative, 0x00200800000a001d, 1, nullptr, STRUCT_OFFSET(AFPSCharacter, GunMeshComponent), Z_Construct_UClass_USkeletalMeshComponent_NoRegister, METADATA_PARAMS(NewProp_GunMeshComponent_MetaData, ARRAY_COUNT(NewProp_GunMeshComponent_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_Mesh1PComponent_MetaData[] = {
+				{ "Category", "Mesh" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "Public/FPSCharacter.h" },
+				{ "ToolTip", "Pawn mesh: 1st person view" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_Mesh1PComponent = { UE4CodeGen_Private::EPropertyClass::Object, "Mesh1PComponent", RF_Public|RF_Transient|RF_MarkAsNative, 0x00200800000a001d, 1, nullptr, STRUCT_OFFSET(AFPSCharacter, Mesh1PComponent), Z_Construct_UClass_USkeletalMeshComponent_NoRegister, METADATA_PARAMS(NewProp_Mesh1PComponent_MetaData, ARRAY_COUNT(NewProp_Mesh1PComponent_MetaData)) };
+			static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[] = {
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_FireAnimation,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_FireSound,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_ProjectileClass,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_CameraComponent,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_GunMeshComponent,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_Mesh1PComponent,
+			};
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<AFPSCharacter>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&AFPSCharacter::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00800080u,
+				nullptr, 0,
+				PropPointers, ARRAY_COUNT(PropPointers),
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AFPSCharacter, 3862916404);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AFPSCharacter(Z_Construct_UClass_AFPSCharacter, &AFPSCharacter::StaticClass, TEXT("/Script/FPSGame"), TEXT("AFPSCharacter"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AFPSCharacter);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
