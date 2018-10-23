c16d70046663acef8881c7bf106d70cf9a5e275b
Tyler McGraw Poff <tyler.poff@outlook.com>
2018-10-23 07:38:38 -0500

    Added the base project for the coop shooter

---
+++ CoopShooterGame/Intermediate/Build/Win64/UE4Editor/Inc/CoopShooterGame/CoopShooterGameGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef COOPSHOOTERGAME_CoopShooterGameGameModeBase_generated_h
+#error "CoopShooterGameGameModeBase.generated.h already included, missing '#pragma once' in CoopShooterGameGameModeBase.h"
+#endif
+#define COOPSHOOTERGAME_CoopShooterGameGameModeBase_generated_h
+
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_RPC_WRAPPERS
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACoopShooterGameGameModeBase(); \
+	friend COOPSHOOTERGAME_API class UClass* Z_Construct_UClass_ACoopShooterGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(ACoopShooterGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/CoopShooterGame"), NO_API) \
+	DECLARE_SERIALIZER(ACoopShooterGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACoopShooterGameGameModeBase(); \
+	friend COOPSHOOTERGAME_API class UClass* Z_Construct_UClass_ACoopShooterGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(ACoopShooterGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/CoopShooterGame"), NO_API) \
+	DECLARE_SERIALIZER(ACoopShooterGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACoopShooterGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACoopShooterGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACoopShooterGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACoopShooterGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACoopShooterGameGameModeBase(ACoopShooterGameGameModeBase&&); \
+	NO_API ACoopShooterGameGameModeBase(const ACoopShooterGameGameModeBase&); \
+public:
+
+
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACoopShooterGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACoopShooterGameGameModeBase(ACoopShooterGameGameModeBase&&); \
+	NO_API ACoopShooterGameGameModeBase(const ACoopShooterGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACoopShooterGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACoopShooterGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACoopShooterGameGameModeBase)
+
+
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_12_PROLOG
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_RPC_WRAPPERS \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_INCLASS \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CoopShooterGame_Source_CoopShooterGame_CoopShooterGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ CoopShooterGame/Source/CoopShooterGame/CoopShooterGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoopShooterGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoopShooterGame, "CoopShooterGame" );
